c08234268f8a9e5796d023e18b0f327f0cdc7ee1
Nelson <nelson.salazar@digitalhouse.com>
2022-05-23 16:23:03 -0300

    Firebase y Formularios

---
+++ clase15_formularioFirebase/myProyect/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import Register from './src/screens/Register';
+import Login from './src/screens/Login';
+
+export default function App() {
+
+  return (
+    <View>
+        <Register />
+        <Login />
+    </View>
+  );
+}
---
+++ clase15_formularioFirebase/myProyect/src/screens/Register.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import {auth} from '../firebase/config';
+
+class Register extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            email: '',
+            password: '',
+            user: ''
+        }
+    }
+
+    register(mail, pass){
+        //Colocar el método de registración de Firebase
+        auth.createUserWithEmailAndPassword(mail, pass)
+            .then( response => console.log(response))
+            .catch( error => console.log(error))      
+    }
+
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.title}>Registro</Text>
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='Email'
+                    onChangeText={text => this.setState({ email: text})}
+                />
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='password'
+                    secureTextEntry={true}
+                    onChangeText={text => this.setState({ password: text})}
+                />
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='Username'
+                    onChangeText={text => this.setState({ user: text})}
+                />
+                <TouchableOpacity onPress={()=>this.register(this.state.email, this.state.password)}>
+                    <Text>Registrarme</Text>
+                </TouchableOpacity>       
+            </View>
+
+        )
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:10,
+        marginTop: 10
+    },
+    title:{
+        marginBottom:20
+    },
+    field:{
+        borderColor: '#dcdcdc',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding:3,
+        marginBottom:8
+
+    }
+})
+
+export default Register;
